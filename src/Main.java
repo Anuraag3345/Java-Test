8fad3eb24ec77ab6acf9e6597b4b21b5346b8da8
Anuraag3345 <blatha@gmail.com>
2023-09-27 15:00:13 -0400

    stuff

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+        public static void main(String[] args) {
+           /* int a = 10;
+            int b = 2;
+            int c=a+b;
+            System.out.println(c); */
+            /*int a[] = {0,1,2,3,4,5,6,7,8,9,10};
+            System.out.println((a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]+a[10])/11);*/
+            /*int x = 10+30;
+            int y = 40-109;
+            double z = 5.5-9.7*9.9;
+            double t = 28938/5.5;
+            int w = 10;
+            w++;
+            System.out.println(x);
+            System.out.println(y);
+            System.out.println(z);
+            System.out.println(t);
+            System.out.println(w);*/
+            /*double price = 90.6;
+            int finalPrice = (int)price;
+            System.out.println(finalPrice);
+            //The decimal gets removed because it is double*/
+
+        }
+    }
---
+++ src/PracticeVariables.java
@@ -0,0 +1,30 @@
+import java.sql.SQLOutput;
+
+public class PracticeVariables {
+    public static void main(String[] args) {
+        //Int
+        /*int a=5;
+        int b=6;
+        int c=b+a;
+        System.out.println(c);*/
+        //Boolean
+       /* boolean x = false;
+        System.out.println(x);*/
+        /*double x = 3.3;
+        System.out.println(x);*/
+        /*String x = "Hi! My name is Anuraag";
+        System.out.println(x);*/
+        /*double a[] = {1,2,3,4};
+        System.out.println((a[0]+a[1]+a[2]+a[3])/4);*/
+        /*int x = 10+10;
+        int y = x+23;
+        int z = x+y;
+        System.out.println(z);*/
+        /*double price = 90.6;
+        int finalPrice = (int)price;
+        System.out.println(finalPrice);
+        //The decimal gets removed because it is double.*/
+
+        }
+    }
+
